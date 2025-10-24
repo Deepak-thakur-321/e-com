4ce3f261a8d71f205913d399c8096d8e4551d6b7
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2025-10-24 16:46:48 +0530

    comment redux imports for deployment

--- src/components/redux/ProductSlice.jsx
+++ src/components/redux/ProductSlice.jsx
@@ -1,35 +1,35 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+// import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
-export const fetchProducts = createAsyncThunk('products/fetch', async () => {
-   const res = await fetch("https://fakestoreapi.com/products");
-   const data = await res.json();
-   return data;
-});
+// export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+//    const res = await fetch("https://fakestoreapi.com/products");
+//    const data = await res.json();
+//    return data;
+// });
 
-const initialState = {
-   items: [],
-   status: "idle",
-   error: null,
-};
+// const initialState = {
+//    items: [],
+//    status: "idle",
+//    error: null,
+// };
 
-const productSlice = createSlice({
-   name: "products",
-   initialState,
-   extraReducers: (builder) => {
-      builder
-         .addCase(fetchProducts.pending, (state) => {
-            state.status = "loading";
-            state.error = null;
-         })
-         .addCase(fetchProducts.fulfilled, (state, action) => {
-            state.items = action.payload;
-            state.status = "success";
-         })
-         .addCase(fetchProducts.rejected, (state, action) => {
-            state.status = "failed";
-            state.error = action.error.message;
-         });
-   },
-});
+// const productSlice = createSlice({
+//    name: "products",
+//    initialState,
+//    extraReducers: (builder) => {
+//       builder
+//          .addCase(fetchProducts.pending, (state) => {
+//             state.status = "loading";
+//             state.error = null;
+//          })
+//          .addCase(fetchProducts.fulfilled, (state, action) => {
+//             state.items = action.payload;
+//             state.status = "success";
+//          })
+//          .addCase(fetchProducts.rejected, (state, action) => {
+//             state.status = "failed";
+//             state.error = action.error.message;
+//          });
+//    },
+// });
 
-export default productSlice.reducer;
+// export default productSlice.reducer;
--- src/components/redux/Store.jsx
+++ src/components/redux/Store.jsx
@@ -1,13 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cartReducer from "../redux/Slice"
-import productsReducer from "../redux/ProductSlice"
+// import { configureStore } from "@reduxjs/toolkit";
+// import cartReducer from "../redux/Slice"
+// import productsReducer from "../redux/ProductSlice"
 
-const store = configureStore({
-   reducer: {
-      cart: cartReducer,
-      products : productsReducer
-   }
-})
+// const store = configureStore({
+//    reducer: {
+//       cart: cartReducer,
+//       products : productsReducer
+//    }
+// })
 
 
-export default store
+// export default store
