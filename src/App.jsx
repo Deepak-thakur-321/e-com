567285d1a85398dc63ffbac3b7620fbfc794dd50
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2025-10-24 16:52:51 +0530

    Remove Redux completely

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Cart from "./components/Product/Cart";
+// import Cart from "./components/Product/Cart";
 import Navbar from "./components/layout/Navbar";
 import Home from "./pages/home/Home";
 import Footer from "./components/layout/Footer";
@@ -14,7 +14,7 @@ function App() {
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+            {/* <Route path="/cart" element={<Cart />} /> */}
             {/* <Route path="/product/:id" element={<ProductCard />} key="product" /> */}
           </Routes>
         </main>
