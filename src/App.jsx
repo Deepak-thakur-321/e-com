7752c039f09552abd63b3471ab209a10b0d2abb5
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2025-10-29 14:35:19 +0530

    fix: fix product description import path

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/layout/Navbar";
 import Home from "./pages/home/Home";
 import Footer from "./components/layout/Footer";
 import Cart from "./components/Product/Cart";
-import ProductDescription from "./pages/description/ProductDescription"; // ✅ New import
+import ProductDescription from "../src/pages/description/ProductDescription"; 
 
 function App() {
   return (
