df031ceef118f22e0db48cd57b7dcba0438b2912
Deepak-thaur-321 <deepakthakur17756@gmail.com>
2025-10-30 14:31:42 +0530

    fix: modify cart Page and make it responsive

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@
   import Navbar from "./components/layout/Navbar";
   import Home from "./pages/home/Home";
   import Footer from "./components/layout/Footer";
-  import Cart from "../src/components/products/CartPage";
+  import CartPage from "../src/components/products/CartPage";
   import ProductDescription from "./pages/description/ProductDescription";
 
   function App() {
@@ -17,7 +17,7 @@
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/cart" element={<Cart />} />
+              <Route path="/cart" element={<CartPage />} />
 
               {/* Placeholder until ProductDescription page is built */}
               <Route path="/product/:id" element={<ProductDescription />} /> </Routes>
